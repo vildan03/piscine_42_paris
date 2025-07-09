72e2fe8d3e6022046aeefe41a21b6d94083919ae
vildan <vildankaraduman03@gmail.com>
2025-07-09 08:29:43 +0200

    ex02 ex03 added

--- c02/ex02/ft_str_is_alpha.c
+++ c02/ex02/ft_str_is_alpha.c
@@ -12,9 +12,23 @@
 
 #include <stdio.h>
 
-int	ft_str_is_alpha(char *str)
-{
+int ft_str_is_alpha(char *str)
+{	
 	int i;
 
-	i = 0;7
-	if(str[i] >= 97 && 
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if(!((str[i] >= 65 &&  str[i] <= 90) || ( str[i] >= 97 && str[i] <= 122)))
+		{
+			return 0;
+		}
+	++i
+	}
+	return 1;
+}
+
+int main(void)
+{
+printf("%d",ft_str_is_alpha(""));
+} 
---
+++ c02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int	ft_str_is_numeric(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{	if(!(str[i] >= 48 && str[i] <= 57))
+		{ 
+			return (0) ;
+		}	
+			++i;
+	}
+	return (1);
+}
+int main(void)
+{ printf("%d",ft_str_is_numeric("000a"));
+}
+	
