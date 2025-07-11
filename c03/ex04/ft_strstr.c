b7eee4839b3d260e9f12718ebb87b63171e0be19
Vildan Karaduman <vikaradu@f0r9s15.clusters.42paris.fr>
2025-07-11 12:23:36 +0200

    ex04 modified

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,45 +6,41 @@
 /*   By: vikaradu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 21:04:04 by vikaradu          #+#    #+#             */
-/*   Updated: 2025/07/10 23:19:38 by vikaradu         ###   ########.fr       */
+/*   Updated: 2025/07/11 12:22:12 by vikaradu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
+
 char	*ft_strstr(char *str, char *to_find)
 {
 	unsigned	int i;
 	unsigned	int f;
-	char	*found;
+
 	i = 0;
 	f = 0;
-
-	if (to_find[f] == '\0')
+	while (to_find[f] != '\0' && str[i] != '\0')
 	{
-		return (str);
-	}
-	while(str[i] != '\0')
-	{
-		while(str[i] != to_find[f])
+		while (str[i + f] ==  to_find[f])
 		{
-			++i;
-			while(str[i] == to_find[f])
-                        {  
-                        found[f] = to_find[f];
-                        ++f;
-                        }
-			++i;
+			++f;
+		}
+		if(str[i+f] != to_find[f] && to_find[f] != '\0')
+		{
+		++i;
 		}
 	}
-		return (found);
-}*/
+	return &str[i];
+}
+			
+
+
 #include <string.h>
 #include <stdio.h>
 int main(void)
 {
-	char str[] = "llo love you";
-	char to_find[] = "";
-//	printf("%s", ft_strstr(str,to_find));
-	printf("%s",strstr(str,to_find));
+	char str[] = "i love you";
+	char to_find[] = "love";
+	printf("%s", ft_strstr(str,to_find));
+//	printf("%s",strstr(str,to_find));
 }
 
 
