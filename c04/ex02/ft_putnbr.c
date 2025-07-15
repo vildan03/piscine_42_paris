d70b73f8f462934cd14138e41d040c6d6f7489b7
Vildan Karaduman <vikaradu@f1r3s15.clusters.42paris.fr>
2025-07-15 16:31:15 +0200

    ex02 ex04 changed

--- c04/ex02/ft_putnbr.c
+++ c04/ex02/ft_putnbr.c
@@ -1,55 +1,42 @@
 #include <unistd.h>
 #include <stdio.h>
-
-long digits(long nbr)
-{
-    int i;
-    i = 0;
-    while (nbr != 0)
-    {
-        nbr = nbr / 10;
-        ++i;
-    }
-    return (i);
-}
-
-void converter_char(unsigned long nbr, char *str)
-{
-int	digit;
-digit = digits(nbr);
-str[digit] = '\0';
-while (digit > 0)
-{
-	str[digit-1] = '0' + nbr % 10;
-	--digit;
-	nbr = nbr / 10;
-}
-}
-
 void ft_putnbr(int nb)
 {	
-	char str[12];
 	long n = nb;
-	int digit;
-	if(n == 0)
+
+	if (n == 0)
 	{
 		write(1,"0",1);
 		return;
 	}
 
 	if (n < 0)
-	{	write(1, "-",1);
-		n = -n;
+	{	
+		if (n == -2147483648)
+		{
+                	write(1,"-2147483648",11);
+			return;
+		}
+		else
+		{
+			write(1, "-",1);
+			n = -n;
+		}
 	}
-	digit = digits(n);
-	converter_char(n,str);
-	write(1,str,digit);
+		
+	if (n >= 10)
+	{
+		ft_putnbr(n / 10);
+	}
+	n = n%10 + '0';
+	write(1,&n,1);
 }
+
 int main(void)
 {
 	ft_putnbr(-42);
 	write(1,"\n",1);
-	ft_putnbr(-2147483647);
+	ft_putnbr(-2147483648);
 	write(1,"\n",1);
 	ft_putnbr(2147483647);
 	write(1,"\n",1);
--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -32,6 +32,38 @@ int is_base_valid(char *base)
 	}
 	return 1;
 }
+void ft_putnbr(int nb)
+{	
+	long n = nb;
+
+	if (n == 0)
+	{
+		write(1,"0",1);
+		return;
+	}
+
+	if (n < 0)
+	{	
+		if (n == -2147483648)
+		{
+                	write(1,"-2147483648",11);
+			return;
+		}
+		else
+		{
+			write(1, "-",1);
+			n = -n;
+		}
+	}
+		
+	if (n >= 10)
+	{
+		ft_putnbr(n / 10);
+	}
+	n = n%10 + '0';
+	write(1,&n,1);
+}
+
 
 void	putnbr_validated_base( int nbr,char *base)
 {
