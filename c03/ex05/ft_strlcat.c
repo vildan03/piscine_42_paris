d2a495c25fd10fbc668e5e9102ffebd8fef26ea9
vildan <vildankaraduman03@gmail.com>
2025-07-16 08:45:01 +0200

    ex05 strlcat

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -25,23 +25,19 @@ unsigned	int	ft_strlcat(char *dest, char *src, unsigned int size)
 		++i;
 	while (src[s] != '\0')
 		++s;
-	while (j < size - i && src[j] != '\0')
+	while (j < size - i -1 && src[j] != '\0')
 	{
 		dest[i + j] = src[j];
 		++j;
 	}
-	while ((i + j) <= size - i)
-	{
-		dest [i + j] = '\0';
-		++j;
-	}
+		dest[i + j] = '\0';
 	return (i + s);
 }
-/*
+
  int main(void)
 {
 char dest[5] = "t";
 char src[3] = "ip";
 printf("%d", ft_strlcat(dest,src,sizeof(dest)));
 }
-*/
+
